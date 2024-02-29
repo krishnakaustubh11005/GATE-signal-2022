90aecbef7c1e2e09cbb27e991c606c613db4215c
Dhyana2904 <155733985+Dhyana2904@users.noreply.github.com>
2024-02-29 21:57:02 +0530

    Add files via upload

---
+++ 2022/IN/16/codes/python2.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load data from file
+data = np.loadtxt('data1.txt')
+
+# Extract time, modulating signal, carrier signal, and amplitude modulated signal
+t = data[:, 0]
+m_t = data[:, 1]
+c_t = data[:, 2]
+s_t = data[:, 3]
+
+# Plot modulating signal
+plt.subplot(3, 1, 1)
+plt.plot(t, m_t, 'b')
+plt.title('Modulating Signal')
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+
+# Plot carrier signal
+plt.subplot(3, 1, 2)
+plt.plot(t, c_t, 'r')
+plt.title('Carrier Signal')
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+
+# Plot amplitude modulated signal
+plt.subplot(3, 1, 3)
+plt.plot(t, s_t, 'g')
+plt.title('Amplitude Modulated Signal')
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+
+plt.tight_layout()
+plt.show()
+
+
